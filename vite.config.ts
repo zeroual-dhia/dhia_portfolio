634397a7b03f959d2c25d887dda1c416c6f21283
dhia <dhiaaeddine.zeroual@ensia.edu.dz>
2025-06-12 12:18:23 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/dhia_portfolio' ,
+  base: '/dhia_portfolio/' ,
   plugins: [
     react(),
     mode === 'development' &&
